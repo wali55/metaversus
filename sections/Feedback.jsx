a2f8723248cb39316b9ae8d77d4876d1b2e02f54
wali55 <walisantunu@gmail.com>
2023-01-24 22:38:14 +0600

    footer_readme

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { fadeIn, staggerContainer,zoomIn } from '../utils/motion';
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
 
 const Feedback = () => (
   <section className={`${styles.paddings} relative z-10`}>
